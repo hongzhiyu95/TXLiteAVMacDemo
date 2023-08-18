fe71b86e61c3c07ca7ba3a73c89c9799dbc9e0dd
yu <einhornyu@163.com>
2023-08-18 16:15:57 +0800

    qtdemo -集成跑通采集（没有预览）

--- QTDemo/LiteAVQTDemo/main.cpp
+++ QTDemo/LiteAVQTDemo/main.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include<TXLiteAVSDK_TRTC_Mac/cpp_interface/ITRTCCloud.h>
 int main(int argc, char *argv[])
 {
+    liteav ::ITRTCCloud *trtc = liteav::ITRTCCloud::getTRTCShareInstance();
+    trtc->startLocalPreview(nullptr);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
