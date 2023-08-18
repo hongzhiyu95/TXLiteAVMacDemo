4a4f4c67cb7d2997c15ac7cfb41f21b8d1489197
yu <einhornyu@163.com>
2023-08-18 12:34:23 +0800

    qt：调整sdk文件目录层级

---
+++ QTDemo/build-LiteAVQTDemo-Desktop_Qt_5_15_2_clang_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/einhorn/GitRepos/TXLiteAVMacDemo/QTDemo/build-LiteAVQTDemo-Desktop_Qt_5_15_2_clang_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/einhorn/Qt/5.15.2/clang_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/einhorn/Qt/5.15.2/clang_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
